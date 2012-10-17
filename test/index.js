7152218dbabc7a0de05fb857e909c4117b2e5063
bmc <brian.m.carlson@gmail.com>
2012-10-16 22:28:49 -0500

    build context in before section

--- test/index.js
+++ test/index.js
@@ -98,56 +98,55 @@ var context = function(cb) {
 };
 
 describe('Context', function() {
+  before(function(done) {
+    var self = this;
+    context(function(context) {
+      self.context = context;
+      done();
+    });
+  });
   describe('parsing variables', function() {
     it('works', function(done) {
-      var stream = new MemoryStream();
-      context(function(context) {
-        var vars = context.variables;
-        expect(vars['agi_network']).ok();
-        expect(vars['agi_network']).to.eql('yes');
-        expect(vars['agi_uniqueid']).to.eql('13507138.14');
-        expect(vars['agi_arg_1']).to.eql('test');
-        done();
-      })
-      it('puts context into waiting state', function() {
-        context(function(context) {
-          expect(context.state).to.eql(state.waiting);
-        });
-      });
+      var vars = this.context.variables;
+      expect(vars['agi_network']).ok();
+      expect(vars['agi_network']).to.eql('yes');
+      expect(vars['agi_uniqueid']).to.eql('13507138.14');
+      expect(vars['agi_arg_1']).to.eql('test');
+      done();
+
+    });
+
+    it('puts context into waiting state', function() {
+      expect(this.context.state).to.eql(state.waiting);
     });
   });
 
   describe('sending command', function() {
     it('writes out', function() {
-      context(function(context) {
-        context.send('EXEC test');
-        expect(context.sent.length).to.eql(1);
-        expect(context.sent.pop()).to.eql('EXEC test');
-      });
+      this.context.send('EXEC test');
+      expect(this.context.sent.length).to.eql(1);
+      expect(this.context.sent.pop()).to.eql('EXEC test');
     });
   });
 
   describe('context.exec', function() {
     it('sends exec command', function() {
-      context(function(context) {
-        context.exec('test', 'bang', 'another');
-        expect(context.sent.pop()).to.eql('EXEC test bang another\n');
-      });
+      this.context.exec('test', 'bang', 'another');
+      expect(this.context.sent.pop()).to.eql('EXEC test bang another\n');
     });
   });
 
   describe('command flow', function() {
     describe('simple success', function() {
       it('works', function(done) {
-        context(function(context) {
-          context.on('response', function() {
-            done();
-          });
-          process.nextTick(function() {
-            context.exec('test', 'bang', 'another');
-            context.stream.write('200');
-            context.stream.write(' result=0\n\n');
-          });
+        var context = this.context;
+        context.on('response', function() {
+          done();
+        });
+        process.nextTick(function() {
+          context.exec('test', 'bang', 'another');
+          context.stream.write('200');
+          context.stream.write(' result=0\n\n');
         });
       });
     });
